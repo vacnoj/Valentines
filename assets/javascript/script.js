faf125fe999ff7fe76e3934d8c9e49623007b8fb
Jon Caviness <Jon@Unknown.domain>
2018-02-14 13:31:41 -0700

    updated

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
    $('#message2').hide();
    $('#poem').hide();
    var valentine;
+   var valentine2;
+   var clickCount = 0;
 
    $('#yes').click(function() {
       $('#message').text('<3 YAY!!! <3');
@@ -10,6 +12,7 @@ $(document).ready(function() {
       if (valentine) {
          $('#answers').hide();
          $('#message2').show();
+         valentine2 = true;
       }
       valentine = true;
    });
@@ -18,4 +21,29 @@ $(document).ready(function() {
       $('body').empty();
       $('body').html('<div class="fine">Fine...</div>');
    });
+
+$('#jon').click(function() { 
+   console.log(valentine);
+
+   if (valentine2) {
+      console.log(clickCount);
+      clickCount++;
+      if (clickCount === 7) {
+         // $('#message2').empty();
+         $('#itinerary').append('<span id="clear"><ol>Cook dinner together</ol><ol>Steak - Main course</ol><ol>Rice - Side dish</ol><ol>Salad - Salad</ol><ol>Chocolate covered strawberries, apples, bananas</ol><ol>Drive Downtown to show off girlfriend</ol><ol>Drive to Lookout mountain</ol><ol>Come home and watch a movie</ol></span>'
+         );
+      } else $('#clear').empty()
+   }
 });
+
+});
+/* Cook dinner together
+    Steak - Main course
+    Rice - Side dish
+    Salad - Salad
+    Chocolate covered strawberries, apples, bananas
+Drive Downtown to show off girlfriend
+Drive to Lookout mountain
+Come home and watch a movie*/
+
+
